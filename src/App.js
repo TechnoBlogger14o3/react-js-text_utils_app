acf6b133c7ea4c09e7bea70effead5ad60502776
Aman Shekhar <aman.shekhar4@gmail.com>
2024-03-12 11:01:58 +0530

    Route Removed

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import TextForm from './components/TextForm';
 import About from './components/About';
 import React, { useState } from 'react';
 import Alert from './components/Alert';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   const [mode, setMode] = useState('light'); // Whether dark mode is enabled or not
@@ -28,11 +28,11 @@ function App() {
   };
 
   return (
-    <Router>
+    // <Router>
       <>
         <Navbar title="TextUtils" mode={mode} aboutText="About Us" toggleMode={toggleMode} key={new Date()} />
         <Alert alert={alert} />
-        <Routes>
+        {/* <Routes>
           <Route
             path="/"
             element={
@@ -42,9 +42,16 @@ function App() {
             }
           />
           <Route path="/about" element={<About mode={mode} />} />
-        </Routes>
+        </Routes> */}
+        <div className="container my-3">
+          <TextForm showAlert={showAlert} heading="Try TextUtils - word counter, character counter, remove extra spaces" mode={mode} />
+          
+        </div>
+      <div className="container my-3">
+      <About mode={mode} />
+      </div>
       </>
-    </Router>
+    // </Router>
   );
 }
 
